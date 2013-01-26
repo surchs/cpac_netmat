6a3de18a0a65a51ae26e91e1939a12036fcbcbf0
sebastian <sebastian.urchs@gmail.com>
2013-01-25 21:26:40 -0500

    changes to test

--- testing/loadHousing.py
+++ testing/loadHousing.py
@@ -8,12 +8,16 @@ small script to load the Boston Housing Data reference dataset
 also turn this into a wrapper to actually run the Network afterwards
 '''
 import sys
+import gzip
+import cPickle
 import numpy as np
-import cpac_netmat.preprocessing.base as pp
+from matplotlib import pyplot as plt
 import cpac_netmat.analysis.base as an
+import cpac_netmat.preprocessing.base as pp
+from matplotlib.backends.backend_pdf import PdfPages as pdf
 
 
-def Main(inFile):
+def Main(inFile, outFile, pdfFile):
     '''
     Load the file, cut it into pieces and print the last line
     '''
@@ -55,6 +59,11 @@ def Main(inFile):
     testNetwork.subjects = subDir
     testNetwork.pheno = 'houseprice'
     testNetwork.featureSelect = 'None'
+    testNetwork.cValue = 1000
+    testNetwork.gridCv = 5
+    testNetwork.gridCores = 1
+    testNetwork.eValue = 0.001
+    testNetwork.kernel = 'rbf'
     # set number of parallel processes in Network
     testNetwork.numberCores = 10
     # make the runs
@@ -63,10 +72,33 @@ def Main(inFile):
     testNetwork.makeRuns()
     # now run the runs
     testNetwork.executeRuns()
-    print('\nGot here')
+    print('\nGot here')    
+    # now save the result
+    outF = gzip.open(outFile, 'wb')
+    cPickle.dump(testNetwork, outF, protocol=2)
+    
+    # and display the rest
+    pPheno = testNetwork.predictedPheno
+    tPheno = testNetwork.truePheno
+    
+    fig4 = plt.figure(4, figsize=(8.5, 11), dpi=150)
+    fig4.suptitle('predicted over true age')
+    
+    tSP4 = fig4.add_subplot(111, title=testNetwork.name)
+    tSP4.plot(tPheno, tPheno)
+    tSP4.plot(tPheno, pPheno, 'co')
+    
+    fig4.subplots_adjust(hspace=0.5, wspace=0.5)
+    
+    pd = pdf(pdfFile)
+    pd.savefig(fig4)
+    plt.close(4)
+    print('Just created ' + pdfFile + '\nAll done here!')
 
 
 if __name__ == '__main__':
     inFile = sys.argv[1]
-    Main(inFile)
+    outFile = sys.argv[2]
+    pdfFile = sys.argv[3]
+    Main(inFile, outFile, pdfFile)
     pass
