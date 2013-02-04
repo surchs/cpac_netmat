60ccfbc84d9dc221415fff5ce25eb64bb07f61bb
sebastian <sebastian.urchs@gmail.com>
2013-02-04 15:13:53 -0500

    new stuff to run locally

---
+++ tools/runGLMfunction.py
@@ -0,0 +1,10 @@
+'''
+Created on Feb 4, 2013
+
+@author: surchs
+
+script to run GLM in python
+'''
+
+if __name__ == '__main__':
+    pass
---
+++ tools/runGLMscript.py
@@ -0,0 +1,12 @@
+'''
+Created on Feb 4, 2013
+
+@author: surchs
+
+a script to run GLM, can run line by line in ipython
+'''
+# defining files
+roiFile = '/home2/surchs/masks/ROIdump/roi_something.nii.gz'
+
+# loading files
+
