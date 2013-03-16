5d617fa0cbdf65aa91423411e12ff9a500e2ae5d
sebastian <sebastian.urchs@gmail.com>
2013-03-16 17:33:27 -0400

    test data

--- secondLine/makeTestConnectomes.py
+++ secondLine/makeTestConnectomes.py
@@ -3,9 +3,18 @@ Created on Mar 15, 2013
 
 @author: sebastian
 '''
+import os
+import copy
 import random
 import numpy as np
 from matplotlib import pyplot as plt
+
+
+def saveConnectome(connectome, pathToSaveFile):
+    '''
+    Save that stuff, diagonal is bullshit obviously
+    '''
+    np.savetxt(pathToSaveFile, connectome, fmt='%.12f')
 
 
 def binarize(value):
@@ -20,7 +29,7 @@ def binarize(value):
     return binarized
 
 
-def makeConnectome():
+def makeConnectome(pathToOutputDir, pathToSubjectPheno):
     '''
     just make a quick range of subjects and plot one of the 'connection'
     '''
@@ -29,6 +38,7 @@ def makeConnectome():
     template = np.zeros((numROIs, numROIs), dtype=float)
     # Make a mask
     mask = np.ones_like(template)
+    empty = np.zeros_like(mask)
     # Get the lower and upper triangle
     upper = np.triu(mask, 1)
     lower = np.tril(mask, -1)
@@ -41,8 +51,6 @@ def makeConnectome():
     ageEffect = np.random.normal(0, 0.01, numConns)
     # Make it range from -1 to +1
     connArray = connArray - 1
-
-
 
     # Prepare container for the ages and connections of the subjects
     ageArray = np.array([])
@@ -63,13 +71,25 @@ def makeConnectome():
         # Make the subjects connectome with noise
         subjectConn = (connArray + connNoise) + (subAge * ageEffect + ageNoise)
 
+        # Turn it into a connectivity matrix
+        template = copy.deepcopy(empty)
+        template[lower == 1] = subjectConn
+        template.T[upper.T == 1] = subjectConn
+
+        # create subject name
+        subName = ('sub_' + str(run))
+        fileName = (subName + '.txt')
+        filePath = os.path.join(pathToOutputDir, fileName)
+        # save the connectome
+        saveConnectome(template, filePath)
+
         # Store the subjects connectome
         if connectomeStack.size == 0:
-            connectomeStack = subjectConn[None, ...]
+            connectomeStack = template[..., None]
         else:
             connectomeStack = np.concatenate((connectomeStack,
-                                              subjectConn[None, ...]),
-                                             axis=0)
+                                              template[..., None]),
+                                             axis=2)
 
         run += 1
 
@@ -77,9 +97,9 @@ def makeConnectome():
     print('Connectome: ' + str(connectomeStack.shape))
 
     # Make a sanity check of connections with age
-    conn1 = connectomeStack[:, 18]
-    conn2 = connectomeStack[:, 800]
-    conn3 = connectomeStack[:, 1082]
+    conn1 = connectomeStack[10, 14, :]
+    conn2 = connectomeStack[100, 13, :]
+    conn3 = connectomeStack[24, 11, :]
     plt.plot(ageArray, conn1, 'g.')
     plt.plot(ageArray, conn2, 'r.')
     plt.plot(ageArray, conn3, 'b.')
@@ -88,6 +108,14 @@ def makeConnectome():
     plt.close()
 
 def Main():
+    # Define outputs
+    pathToOutputDir = '/home2/surchs/secondLine/connectomes/testing'
+    pathToSubjectPheno = '/home2/surchs/secondLine/configs/testing/sub100pheno.csv'
+
+
+    # Run the connectome generator
+    connectomeStack, ageStack = makeConnectome(pathToOutputDir,
+                                               pathToSubjectPheno)
 
     pass
 
